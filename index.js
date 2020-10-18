ed3356b050f62ac6d08b5fcc37c28b6583254597
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-10-18 15:58:18 -0500

    Done.

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ function createFareMultiplier(int) { return function(fare) { return fare * int }
 
 // function fareDoubler(fare) { return fare * 2 }
 // function fareDoubler(fare) { return createFareMultiplier(2)(fare) }
-const fareDoubler = (fare) => { return createFareMultiplier(2)(fare) }
+// const fareDoubler = (fare) => { return createFareMultiplier(2)(fare) }
+const fareDoubler = createFareMultiplier(2)
 
 // function fareTripler(fare) {return fare * 3 }
 // function fareTripler(fare) { return createFareMultiplier(3)(fare) }
-const fareTripler = (fare) => { return createFareMultiplier(3)(fare) }
+// const fareTripler = (fare) => { return createFareMultiplier(3)(fare) }
+const fareTripler = createFareMultiplier(3)
 
 function selectDifferentDrivers(array, func) { return func(array) }
