4544e495532a41bad8d3e87269dee620f35fa9c4
Shamel Lakin <christlife4u@gmail.com>
2020-10-19 16:36:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+// const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+};
+// returnFirstTwoDrivers()
+
+const returnLastTwoDrivers = function (drivers) {
+return drivers.slice(2, 4);
+};
+returnLastTwoDrivers()
+
+const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
